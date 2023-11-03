46ec1d419a8df7e78188dd0ab6a5d7807e19d0e7
Dorian <dorianturle@hotmail.fr>
2023-11-03 17:03:41 +0100

    create Role model

---
+++ app/Models/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        "alias"
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->create([
+                'name' => 'Admin',
+                'email' => 'admin@example.fr',
+                'role_id' => Role::query()->whereAlias('admin')->first()->id,
+            ]);
+    }
+}
